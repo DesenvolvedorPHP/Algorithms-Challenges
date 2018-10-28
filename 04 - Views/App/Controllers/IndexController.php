dbee5f768a704c16abe8cccf456070855dafb4de
Fernando Britto <fernandobritto.web@gmail.com>
2018-10-27 22:25:25 -0200

    Exibindo as Views

---
+++ 04 - Views/App/Controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+
+class IndexController {
+    
+    public function index()
+    {
+        $cars = array("Mustang", "Ferrari");
+        include_once "../App/Views/index/index.phtml";
+    }
+    
+    public function contact()
+    {
+        $cars = array("Mustang", "Ferrari"); 
+        include_once "../App/Views/index/contact.phtml";
+    }
+}
